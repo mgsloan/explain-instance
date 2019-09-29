05932ffe9a24229cebe9ebe317be87bffec46fd2
Michael Sloan <mgsloan@gmail.com>
2019-09-29 00:08:51 -0600

    Add a couple examples from blogpost

---
+++ examples/MonoidMaybeInt.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverlappingInstances #-}
+
+import ExplainInstance
+import Data.Monoid
+
+$(explainInstanceError [t| Monoid (Maybe Int) |])
